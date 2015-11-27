cd295462d914f7c4aa2c5a3e2c2284f7a2b26cc9
Cayle Sharrock <cayle@nimbustech.biz>
2015-11-27 10:49:36 +0200

    make package pyPI ready

--- setup.py
+++ setup.py
@@ -1,15 +1,29 @@
+import ez_setup
+ez_setup.use_setuptools()
+
 from setuptools import setup, find_packages
 import pybitx
 
 setup(
     name='mypackage',
     version=pybitx.__version__,
-    packages=find_packages(),
+    packages=find_packages(exclude=['tests']),
     description='A BitX API for Python',
     author='Cayle Sharrock',
     author_email='cayle@nimbustech.biz',
-    url='https://github.com/CjS77/pybitx',  # use the URL to the github repo
-    download_url='https://github.com/CjS77/pybitx/tarball/0.1',  # I'll explain this in a second
-    keywords=['BitX', 'Bitcoin', 'exchange', 'API'],  # arbitrary keywords
+    scripts=['demo.py'],
+    install_requires=[
+        'futures>=3.0.3',
+        'nose>=1.3.7',
+        'requests>=2.8.1'
+    ],
+    license='MIT',
+    url='https://github.com/CjS77/pybitx',
+    download_url='https://github.com/CjS77/pybitx/tarball/%s' % (pybitx.__version__, ),
+    keywords='BitX Bitcoin exchange API',
     classifiers=[],
+    test_suite='tests',
+    tests_require=[
+        'requests-mock>=0.7.0'
+    ]
 )
