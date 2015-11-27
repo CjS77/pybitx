5384876e968dd4f74a73f3495dbe9f94023fbdc2
Cayle Sharrock <cayle@nimbustech.biz>
2015-11-27 07:51:00 +0200

    add more calls and auth
    more calls added
    basic auth added
    requirements.txt added

--- api.py
+++ api.py
@@ -10,8 +10,8 @@ __version__ = '0.1.0'
 # --------------------------- constants -----------------------
 
 class BitXAPIError(ValueError):
-    def __init__(self, url, response):
-        self.url = url
+    def __init__(self, response):
+        self.url = response.url
         self.code = response.status_code
         self.message = response.text
 
@@ -22,7 +22,7 @@ class BitXAPIError(ValueError):
 class BitX:
     def __init__(self, key, secret, options={}):
         self.options = options
-        self.auth = "%s:%s" % (key, secret)
+        self.auth = (key, secret)
         if 'hostname' in options:
             self.hostname = options['hostname']
         else:
@@ -43,34 +43,59 @@ class BitX:
         self._executor.shutdown(wait=True)
         log.info('MultiThreadPool has shutdown')
 
-    def construct_url(self, call, kind='basic'):
-        if kind == 'basic':
+    def construct_url(self, call):
             base = self.hostname
             if self.port != 443:
                 base += ':%d' % (self.port,)
             return "https://%s/api/1/%s" % (base, call)
 
-    def api_request(self, call, params):
+    def api_request(self, call, params, kind='auth'):
         """
         General API request. Generally, use the convenience functions below
+        :param kind: the type of request to make. 'auth' makes an authenticated call; 'basic' is unauthenticated
         :param call: the API call to make
         :param params: a dict of query parameters
         :return: a json response, a BitXAPIError is thrown if the api returns with an error
         """
         url = self.construct_url(call)
-        future = self._executor.submit(requests.get, url, params)
+        if kind == 'auth':
+            future = self._executor.submit(requests.get, url, params, headers=self.headers, auth=self.auth)
+        else:
+            future = self._executor.submit(requests.get, url, params, headers=self.headers)
         response = future.result(timeout=self.timeout)
-        result = response.json()
-        if 'error' in result:
-            raise BitXAPIError(url, response)
+        try:
+            result = response.json()
+        except ValueError:
+            result = {'error': 'No JSON content returned'}
+        if response.status_code != 200 or 'error' in result:
+            raise BitXAPIError(response)
         else:
             return result
 
-    def get_ticker(self):
-        params = {
-            'pair': self.pair
-        }
-        return self.api_request('ticker', params)
+    def get_ticker(self, kind='auth'):
+        params = {'pair': self.pair}
+        return self.api_request('ticker', params, kind=kind)
 
-    def get_all_tickers(self):
-        return self.api_request('tickers', None)
+    def get_all_tickers(self, kind='auth'):
+        return self.api_request('tickers', None, kind=kind)
+
+    def get_order_book(self, kind='auth'):
+        params = {'pair': self.pair}
+        return self.api_request('orderbook', params, kind=kind)
+
+    def get_trades(self, kind='auth'):
+        params = {'pair': self.pair}
+        return self.api_request('trades', params, kind=kind)
+
+    def get_orders(self, state=None, kind='auth'):
+        """
+        Returns a list of the most recently placed orders. You can specify an optional state='PENDING' parameter to
+        restrict the results to only open orders. You can also specify the market by using the optional pair parameter.
+        The list is truncated after 100 items.
+        :param state: String optional 'COMPLETE', 'PENDING', or None (default)
+        :return:
+        """
+        params = {'pair': self.pair}
+        if state is not None:
+            params['state'] = state
+        return self.api_request('listorders', params, kind=kind)
--- tests/test_api.py
+++ tests/test_api.py
@@ -1,6 +1,14 @@
 import unittest
 import requests_mock
+import api
 from api import BitX, BitXAPIError
+import base64
+
+
+def make_auth_header(auth):
+    s = ':'.join(auth)
+    k = base64.b64encode(s)
+    return 'Basic %s' % (k,)
 
 
 class TestBitX(unittest.TestCase):
@@ -26,7 +34,7 @@ class TestBitX(unittest.TestCase):
         self.assertEqual(api.hostname, options['hostname'])
         self.assertEqual(api.port, options['port'])
         self.assertEqual(api.pair, options['pair'])
-        self.assertEqual(api.auth, key + ':' + secret)
+        self.assertEqual(api.auth, (key, secret))
 
     def testConstructURL(self):
         api = BitX('', '')
@@ -65,9 +73,22 @@ class TestAPICalls(unittest.TestCase):
             'hostname': 'api.dummy.com',
             'pair': 'XBTZAR'
         }
-        key = 'cnz2yjswbv3jd'
-        secret = '0hydMZDb9HRR3Qq-iqALwZtXLkbLR4fWxtDZvkB9h4I'
+        key = 'mykey'
+        secret = 'mysecret'
         self.api = BitX(key, secret, options)
+        self.auth_string = make_auth_header(self.api.auth)
+
+
+    @requests_mock.Mocker()
+    def testHeaders(self, m):
+        headers = {
+            'Accept': 'application/json',
+            'Accept-Charset': 'utf-8',
+            'User-Agent': 'py-bitx v' + api.__version__
+        }
+        m.get('https://api.dummy.com/api/1/ticker?pair=XBTZAR', json={'success': True}, request_headers=headers)
+        result = self.api.get_ticker()
+        self.assertTrue(result['success'])
 
     @requests_mock.Mocker()
     def testTickerCall(self, m):
@@ -92,7 +113,7 @@ class TestAPICalls(unittest.TestCase):
             self.fail('Exception not thrown')
         except BitXAPIError as e:
             self.assertEqual(e.code, 200)
-            self.assertEqual(e.url, url)
+            self.assertEqual(e.url, url + '?pair=' + self.api.pair)
 
     @requests_mock.Mocker()
     def testAllTickers(self, m):
@@ -107,6 +128,96 @@ class TestAPICalls(unittest.TestCase):
         m.get('https://api.dummy.com/api/1/tickers', json=response)
         result = self.api.get_all_tickers()
         self.assertDictEqual(result, response)
+
+    @requests_mock.Mocker()
+    def testOrderBook(self, m):
+        response = {
+            "timestamp": 1366305398592,
+            "bids": [
+                {
+                    "volume": "0.10",
+                    "price": "1100.00"
+                },
+                {
+                    "volume": "0.10",
+                    "price": "1000.00"
+                },
+                {
+                    "volume": "0.10",
+                    "price": "900.00"
+                }
+            ],
+            "asks": [
+                {
+                    "volume": "0.10",
+                    "price": "1180.00"
+                },
+                {
+                    "volume": "0.10",
+                    "price": "2000.00"
+                }
+            ]
+        }
+        m.get('https://api.dummy.com/api/1/orderbook', json=response)
+        result = self.api.get_order_book()
+        self.assertDictEqual(result, response)
+
+    @requests_mock.Mocker()
+    def testTrades(self, m):
+        response = {
+            "trades": [
+                {
+                    "volume": "0.10",
+                    "timestamp": 1366052621774,
+                    "price": "1000.00"
+                },
+                {
+                    "volume": "1.20",
+                    "timestamp": 1366052621770,
+                    "price": "1020.50"
+                }
+            ]
+        }
+        m.get('https://api.dummy.com/api/1/trades', json=response)
+        result = self.api.get_trades()
+        self.assertDictEqual(result, response)
+
+    @requests_mock.Mocker()
+    def testListOrdersAuth(self, m):
+        response = {
+            "orders": [
+                {
+                    "base": "0.027496",
+                    "counter": "81.140696",
+                    "creation_timestamp": 1423990327333,
+                    "expiration_timestamp": 0,
+                    "fee_base": "0.00",
+                    "fee_counter": "0.00",
+                    "limit_price": "2951.00",
+                    "limit_volume": "0.027496",
+                    "order_id": "BXF3J88PZAYGXH7",
+                    "pair": "XBTZAR",
+                    "state": "COMPLETE",
+                    "type": "ASK",
+
+                }
+            ]
+        }
+        url = 'https://api.dummy.com/api/1/listorders?pair=XBTZAR'
+        m.get(url, json=response, request_headers={'Authorization': self.auth_string})
+        result = self.api.get_orders()
+        self.assertDictEqual(result, response)
+
+    @requests_mock.Mocker()
+    def testListOrdersUnAuth(self, m):
+        url = 'https://api.dummy.com/api/1/listorders?pair=XBTZAR'
+        m.get(url, text='', status_code=401)
+        try:
+            self.api.get_orders(kind='basic')
+            self.fail('Exception not thrown')
+        except BitXAPIError as e:
+            self.assertEqual(e.code, 401)
+            self.assertEqual(e.url, url)
 
 
 def main():
